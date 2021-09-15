bf3a23c0c2c49dcf7674a623ea0d288fc50c9f0d
Matthew Lilley <hello@matthewlilley.com>
2021-09-16 00:26:25 +0100

    feat: init bentobox

---
+++ packages/bentobox/src/functions/index.ts
@@ -0,0 +1,14 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { Zero } from "@ethersproject/constants";
+
+export function toAmount(token: any, shares: BigNumber): BigNumber {
+  return BigNumber.from(token.bentoShare).gt(0)
+    ? shares.mul(token.bentoAmount).div(token.bentoShare)
+    : Zero;
+}
+
+export function toShare(token: any, amount: BigNumber): BigNumber {
+  return BigNumber.from(token.bentoAmount).gt(0)
+    ? amount.mul(token.bentoShare).div(token.bentoAmount)
+    : Zero;
+}
