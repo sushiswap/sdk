be90b30101cc772d4269cec1801004448223572c
Matthew Lilley <hello@matthewlilley.com>
2021-09-15 17:15:45 +0100

    feat: further hybrid

--- packages/trident/src/entities/HybridPool.ts
+++ packages/trident/src/entities/HybridPool.ts
@@ -1,6 +1,21 @@
-import { ChainId, ChainKey, CurrencyAmount, Token } from "@sushiswap/core-sdk";
+import {
+  ChainId,
+  ChainKey,
+  CurrencyAmount,
+  InsufficientInputAmountError,
+  InsufficientReservesError,
+  MINIMUM_LIQUIDITY,
+  ONE,
+  Price,
+  Token,
+  ZERO,
+  _1000,
+  _997,
+  sqrt,
+} from "@sushiswap/core-sdk";
 
 import { Fee } from "../enums";
+import JSBI from "jsbi";
 import all from "@sushiswap/trident/exports/all.json";
 import { computeHybridPoolAddress } from "../functions";
 import invariant from "tiny-invariant";
@@ -67,6 +82,41 @@ export class HybridPool {
   }
 
   /**
+   * Returns the current mid price of the pair in terms of token0, i.e. the ratio of reserve1 to reserve0
+   */
+  public get token0Price(): Price<Token, Token> {
+    const result = this.tokenAmounts[1].divide(this.tokenAmounts[0]);
+    return new Price(
+      this.token0,
+      this.token1,
+      result.denominator,
+      result.numerator
+    );
+  }
+
+  /**
+   * Returns the current mid price of the pair in terms of token1, i.e. the ratio of reserve0 to reserve1
+   */
+  public get token1Price(): Price<Token, Token> {
+    const result = this.tokenAmounts[0].divide(this.tokenAmounts[1]);
+    return new Price(
+      this.token1,
+      this.token0,
+      result.denominator,
+      result.numerator
+    );
+  }
+
+  /**
+   * Return the price of the given token in terms of the other token in the pair.
+   * @param token token to return price of
+   */
+  public priceOf(token: Token): Price<Token, Token> {
+    invariant(this.involvesToken(token), "TOKEN");
+    return token.equals(this.token0) ? this.token0Price : this.token1Price;
+  }
+
+  /**
    * Returns the chain ID of the tokens in the pair.
    */
   public get chainId(): number {
@@ -92,5 +142,146 @@ export class HybridPool {
   public reserveOf(token: Token): CurrencyAmount<Token> {
     invariant(this.involvesToken(token), "TOKEN");
     return token.equals(this.token0) ? this.reserve0 : this.reserve1;
+  }
+
+  public getOutputAmount(
+    inputAmount: CurrencyAmount<Token>
+  ): [CurrencyAmount<Token>, HybridPool] {
+    invariant(this.involvesToken(inputAmount.currency), "TOKEN");
+    if (
+      JSBI.equal(this.reserve0.quotient, ZERO) ||
+      JSBI.equal(this.reserve1.quotient, ZERO)
+    ) {
+      throw new InsufficientReservesError();
+    }
+    const inputReserve = this.reserveOf(inputAmount.currency);
+    const outputReserve = this.reserveOf(
+      inputAmount.currency.equals(this.token0) ? this.token1 : this.token0
+    );
+    const inputAmountWithFee = JSBI.multiply(inputAmount.quotient, _997);
+    const numerator = JSBI.multiply(inputAmountWithFee, outputReserve.quotient);
+    const denominator = JSBI.add(
+      JSBI.multiply(inputReserve.quotient, _1000),
+      inputAmountWithFee
+    );
+    const outputAmount = CurrencyAmount.fromRawAmount(
+      inputAmount.currency.equals(this.token0) ? this.token1 : this.token0,
+      JSBI.divide(numerator, denominator)
+    );
+    if (JSBI.equal(outputAmount.quotient, ZERO)) {
+      throw new InsufficientInputAmountError();
+    }
+    return [
+      outputAmount,
+      new HybridPool(
+        inputReserve.add(inputAmount),
+        outputReserve.subtract(outputAmount),
+        this.fee,
+        this.a
+      ),
+    ];
+  }
+
+  public getInputAmount(
+    outputAmount: CurrencyAmount<Token>
+  ): [CurrencyAmount<Token>, HybridPool] {
+    invariant(this.involvesToken(outputAmount.currency), "TOKEN");
+    if (
+      JSBI.equal(this.reserve0.quotient, ZERO) ||
+      JSBI.equal(this.reserve1.quotient, ZERO) ||
+      JSBI.greaterThanOrEqual(
+        outputAmount.quotient,
+        this.reserveOf(outputAmount.currency).quotient
+      )
+    ) {
+      throw new InsufficientReservesError();
+    }
+
+    const outputReserve = this.reserveOf(outputAmount.currency);
+    const inputReserve = this.reserveOf(
+      outputAmount.currency.equals(this.token0) ? this.token1 : this.token0
+    );
+    const numerator = JSBI.multiply(
+      JSBI.multiply(inputReserve.quotient, outputAmount.quotient),
+      _1000
+    );
+    const denominator = JSBI.multiply(
+      JSBI.subtract(outputReserve.quotient, outputAmount.quotient),
+      _997 // 3%
+    );
+    const inputAmount = CurrencyAmount.fromRawAmount(
+      outputAmount.currency.equals(this.token0) ? this.token1 : this.token0,
+      JSBI.add(JSBI.divide(numerator, denominator), ONE)
+    );
+    return [
+      inputAmount,
+      new HybridPool(
+        inputReserve.add(inputAmount),
+        outputReserve.subtract(outputAmount),
+        this.fee,
+        this.a
+      ),
+    ];
+  }
+
+  public getLiquidityMinted(
+    totalSupply: CurrencyAmount<Token>,
+    tokenAmountA: CurrencyAmount<Token>,
+    tokenAmountB: CurrencyAmount<Token>
+  ): CurrencyAmount<Token> {
+    invariant(totalSupply.currency.equals(this.liquidityToken), "LIQUIDITY");
+    const tokenAmounts = tokenAmountA.currency.sortsBefore(
+      tokenAmountB.currency
+    ) // does safety checks
+      ? [tokenAmountA, tokenAmountB]
+      : [tokenAmountB, tokenAmountA];
+    invariant(
+      tokenAmounts[0].currency.equals(this.token0) &&
+        tokenAmounts[1].currency.equals(this.token1),
+      "TOKEN"
+    );
+
+    let liquidity: JSBI;
+    if (JSBI.equal(totalSupply.quotient, ZERO)) {
+      liquidity = JSBI.subtract(
+        sqrt(JSBI.multiply(tokenAmounts[0].quotient, tokenAmounts[1].quotient)),
+        MINIMUM_LIQUIDITY
+      );
+    } else {
+      const amount0 = JSBI.divide(
+        JSBI.multiply(tokenAmounts[0].quotient, totalSupply.quotient),
+        this.reserve0.quotient
+      );
+      const amount1 = JSBI.divide(
+        JSBI.multiply(tokenAmounts[1].quotient, totalSupply.quotient),
+        this.reserve1.quotient
+      );
+      liquidity = JSBI.lessThanOrEqual(amount0, amount1) ? amount0 : amount1;
+    }
+    if (!JSBI.greaterThan(liquidity, ZERO)) {
+      throw new InsufficientInputAmountError();
+    }
+    return CurrencyAmount.fromRawAmount(this.liquidityToken, liquidity);
+  }
+
+  public getLiquidityValue(
+    token: Token,
+    totalSupply: CurrencyAmount<Token>,
+    liquidity: CurrencyAmount<Token>
+  ): CurrencyAmount<Token> {
+    invariant(this.involvesToken(token), "TOKEN");
+    invariant(totalSupply.currency.equals(this.liquidityToken), "TOTAL_SUPPLY");
+    invariant(liquidity.currency.equals(this.liquidityToken), "LIQUIDITY");
+    invariant(
+      JSBI.lessThanOrEqual(liquidity.quotient, totalSupply.quotient),
+      "LIQUIDITY"
+    );
+    return CurrencyAmount.fromRawAmount(
+      token,
+      JSBI.divide(
+        JSBI.multiply(liquidity.quotient, this.reserveOf(token).quotient),
+        totalSupply.quotient
+      )
+    );
   }
 }
