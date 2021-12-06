03917abd9777afe2b9b0c7e0688f79e5f9d470ad
ilya <bretnwood@gmail.com>
2021-12-06 15:10:33 +0300

    + convertTinesSingleRouteToLegacyRoute function

--- packages/trident/src/functions/wrapTines.ts
+++ packages/trident/src/functions/wrapTines.ts
@@ -1,5 +1,5 @@
 import { BigNumber } from "@ethersproject/bignumber";
-import { Pair, Token } from "@sushiswap/core-sdk";
+import { Currency, Pair, Route, Token } from "@sushiswap/core-sdk";
 import {
   ConstantProductRPool, 
   findMultiRouteExactIn as TinesFindMultiRouteExactIn, 
@@ -107,4 +107,22 @@ export function findSingleRouteExactOut(
     baseToken as RToken,
     gasPrice
   )
+}
+
+export function convertTinesSingleRouteToLegacyRoute<TInput extends Currency, TOutput extends Currency>(
+  route: MultiRoute,
+  allPairs: Pair[], 
+  input: TInput, 
+  output: TOutput
+): Route<TInput, TOutput> {
+  const pairHash = new Map<string, Pair>()
+  allPairs.forEach(p => pairHash.set(p.liquidityToken.address, p))
+  const pairs = route.legs.map(l => {
+    const pair = pairHash.get(l.poolAddress)
+    if (pair === undefined) {
+      throw new Error('Internal Error 119')
+    }
+    return pair
+  })
+  return new Route(pairs, input, output)
 }
