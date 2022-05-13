f333e24104f5d6b3782cdcf3b0af08f927fcdaf2
matthewlilley <hello@matthewlilley.com>
2022-05-13 16:28:02 +0100

    fix(trident-sdk): alias older version of trident for matic bytecode

--- packages/trident-sdk/src/functions/computeConstantProductPoolAddress.ts
+++ packages/trident-sdk/src/functions/computeConstantProductPoolAddress.ts
@@ -2,7 +2,11 @@ import EXPORTS from '@sushiswap/trident/exports/all.json'
 import { Fee } from '../enums/Fee'
 import { ChainId, Token } from '@sushiswap/core-sdk'
 import { computePoolInitCodeHash } from './computePoolInitCodeHash'
+
+import constantProductPoolArtifactPreview from 'trident-preview/artifacts/contracts/pool/constant-product/ConstantProductPool.sol/ConstantProductPool.json'
+
 import constantProductPoolArtifact from '@sushiswap/trident/artifacts/contracts/pool/constant-product/ConstantProductPool.sol/ConstantProductPool.json'
+
 import { defaultAbiCoder } from '@ethersproject/abi'
 import { getCreate2Address } from '@ethersproject/address'
 import { keccak256 } from '@ethersproject/solidity'
@@ -32,7 +36,7 @@ export const computeConstantProductPoolAddress = ({
   const CONSTANT_PRODUCT_POOL_INIT_CODE_HASH =
     token0.chainId === ChainId.MATIC
       ? computePoolInitCodeHash({
-          creationCode: constantProductPoolArtifact.bytecode,
+          creationCode: constantProductPoolArtifactPreview.bytecode,
           deployData,
           masterDeployerAddress: (EXPORTS as any)[token0.chainId][0].contracts.MasterDeployer.address,
         })
