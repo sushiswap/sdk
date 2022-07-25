b5682a8ec224ce3de22432b61d3e43fd4a499c58
Matthew Lilley <hello@matthewlilley.com>
2022-07-25 23:00:42 +0000

    chore: version hash

--- packages/bentobox-sdk/src/functions/index.ts
+++ packages/bentobox-sdk/src/functions/index.ts
@@ -1,9 +1,25 @@
 import { ZERO, JSBI, Rebase } from '@sushiswap/core-sdk'
 
-export function toAmount(token: Rebase, shares: JSBI): JSBI {
-  return JSBI.GT(token.base, 0) ? JSBI.divide(JSBI.multiply(shares, token.elastic), token.base) : ZERO
+export function toAmount(rebase: Rebase, shares: JSBI, roundUp = false): JSBI {
+  if (JSBI.EQ(rebase.base, ZERO)) return shares
+
+  const elastic = JSBI.divide(JSBI.multiply(shares, rebase.elastic), rebase.base)
+
+  if (roundUp && JSBI.LT(JSBI.divide(JSBI.multiply(elastic, rebase.base), rebase.elastic), shares)) {
+    return JSBI.add(elastic, JSBI.BigInt(1))
+  }
+
+  return elastic
 }
 
-export function toShare(token: Rebase, amount: JSBI): JSBI {
-  return JSBI.GT(token.elastic, 0) ? JSBI.divide(JSBI.multiply(amount, token.base), token.elastic) : ZERO
+export function toShare(rebase: Rebase, amount: JSBI, roundUp = false): JSBI {
+  if (JSBI.EQ(rebase.elastic, ZERO)) return amount
+
+  const base = JSBI.divide(JSBI.multiply(amount, rebase.base), rebase.elastic)
+
+  if (roundUp && JSBI.LT(JSBI.divide(JSBI.multiply(base, rebase.elastic), rebase.base), amount)) {
+    return JSBI.add(base, JSBI.BigInt(1))
+  }
+
+  return base
 }
