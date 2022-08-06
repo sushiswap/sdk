d29cf04e2afa74b0bf4b6296a892ff95745454ad
sam bacha <sam@manifoldfinance.com>
2022-08-06 06:05:00 -0700

    build lockfiles

--- packages/core-sdk/src/router.ts
+++ packages/core-sdk/src/router.ts
@@ -35,7 +35,7 @@ export interface TradeOptions {
 export interface TradeOptionsDeadline extends Omit<TradeOptions, 'ttl'> {
   /**
    * When the transaction expires.
-   * This is an atlernate to specifying the ttl, for when you do not want to use local time.
+   * This is an alternate to specifying the ttl, for when you do not want to use local time.
    */
   deadline: number
 }
